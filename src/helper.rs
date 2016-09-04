13e5da4f6324a51313c9e14a1bf218ac0c658094
David Tolnay <dtolnay@gmail.com>
2016-09-04 16:18:34 -0700

    Rewrite punct macro in Rust

--- src/helper.rs
+++ src/helper.rs
@@ -4,8 +4,23 @@ use nom::{self, IResult};
 
 macro_rules! punct {
     ($i:expr, $punct:expr) => {
-        complete!($i, preceded!(opt!(call!(::nom::multispace)), tag_s!($punct)))
+        $crate::helper::punct($i, $punct)
     };
+}
+
+pub fn punct<'a>(input: &'a str, token: &'static str) -> IResult<&'a str, &'a str> {
+    let mut chars = input.char_indices();
+    while let Some((i, ch)) = chars.next() {
+        if !ch.is_whitespace() {
+            return if input[i..].starts_with(token) {
+                let end = i + token.len();
+                IResult::Done(&input[end..], &input[i..end])
+            } else {
+                IResult::Error(nom::Err::Position(nom::ErrorKind::TagStr, input))
+            };
+        }
+    }
+    IResult::Error(nom::Err::Position(nom::ErrorKind::TagStr, input))
 }
 
 macro_rules! option (
