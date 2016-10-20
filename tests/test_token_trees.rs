095a42b93b35f8ab498d877ddef2421ddf113c2b
Simon Sapin <simon.sapin@exyr.org>
2016-10-20 15:54:23 +0200

    Add parse_token_trees.
    Token trees are much simpler than an AST.
    This makes them easier to work with when they are sufficient
    for the task at hand, such as for example [expanding a procedural macro](
    https://github.com/servo/html5ever/pull/217).

---
+++ tests/test_token_trees.rs
@@ -0,0 +1,62 @@
+extern crate syn;
+use syn::TokenTree::{self, Token};
+use syn::DelimToken::*;
+use syn::Token::*;
+
+#[test]
+fn test_struct() {
+    let raw = "
+        #[derive(Debug, Clone)]
+        pub struct Item {
+            pub ident: Ident,
+            pub attrs: Vec<Attribute>,
+        }
+    ";
+
+    let expected = vec![
+        Token(Pound),
+        delimited(Bracket, vec![
+            ident("derive"),
+            delimited(Paren, vec![
+                ident("Debug"),
+                Token(Comma),
+                ident("Clone"),
+            ]),
+        ]),
+        ident("pub"),
+        ident("struct"),
+        ident("Item"),
+        delimited(Brace, vec![
+            ident("pub"),
+            ident("ident"),
+            Token(Colon),
+            ident("Ident"),
+            Token(Comma),
+
+            ident("pub"),
+            ident("attrs"),
+            Token(Colon),
+            ident("Vec"),
+            Token(Lt),
+            ident("Attribute"),
+            Token(Gt),
+            Token(Comma),
+        ]),
+    ];
+
+    let result = syn::parse_token_trees(raw).unwrap();
+    if result != expected {
+        panic!("{:#?}\n!=\n{:#?}", result, expected);
+    }
+}
+
+fn delimited(delim: syn::DelimToken, tts: Vec<TokenTree>) -> TokenTree {
+    TokenTree::Delimited(syn::Delimited {
+        delim: delim,
+        tts: tts,
+    })
+}
+
+fn ident(s: &str) -> TokenTree {
+    TokenTree::Token(Ident(syn::Ident::new(s)))
+}
