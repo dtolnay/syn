25231e85d78c9d0222a1b771136757a109f6182e
Nika Layzell <nika@thelayzells.com>
2017-12-05 12:48:07 -0500

    Add some simple should_parse tests

---
+++ tests/test_should_parse.rs
@@ -0,0 +1,27 @@
+//! This module contains some simple tests where we can write strings which we 
+
+extern crate syn;
+
+macro_rules! should_parse {
+    ($name:ident, { $($in:tt)* }) => {
+        #[test]
+        fn $name() {
+            // Make sure we can parse the file!
+            syn::parse_file(stringify!($($in)*)).unwrap();
+        }
+    }
+}
+
+should_parse!(generic_associated_type, {
+    impl Foo {
+        type Item<'a> = &'a i32;
+        fn foo<'a>(&'a self) -> Self::Item<'a> {}
+    }
+});
+
+should_parse!(const_generics_use, {
+    type X = Foo<5>;
+    type Y = Foo<"foo">;
+    type Z = Foo<X>;
+    type W = Foo<{ X + 10 }>;
+});
