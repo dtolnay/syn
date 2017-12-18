ec5b52e9c836d4fb41bc2e4762d8196ee9a8edb3
David Tolnay <dtolnay@gmail.com>
2017-12-17 22:42:32 -0800

    Avoid interleaving errorf in tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -13,7 +13,8 @@ macro_rules! errorf {
     ($($tt:tt)*) => {
         {
             use ::std::io::Write;
-            write!(::std::io::stderr(), $($tt)*).unwrap();
+            let stderr = ::std::io::stderr();
+            write!(stderr.lock(), $($tt)*).unwrap();
         }
     };
 }
