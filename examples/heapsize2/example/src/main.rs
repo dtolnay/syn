810e97bcc0ed4542de789730ac74659a1776458c
David Tolnay <dtolnay@gmail.com>
2018-01-02 09:24:32 -0800

    Fix hygiene in heapsize2 example

--- examples/heapsize2/example/src/main.rs
+++ examples/heapsize2/example/src/main.rs
@@ -1,9 +1,6 @@
 #[macro_use]
 extern crate heapsize_derive;
 
-extern crate heapsize;
-use heapsize::HeapSize;
-
 #[derive(HeapSize)]
 struct Demo<'a, T: ?Sized> {
     a: Box<T>,
--- examples/heapsize2/heapsize_derive/src/lib.rs
+++ examples/heapsize2/heapsize_derive/src/lib.rs
@@ -1,17 +1,11 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-extern crate proc_macro2;
-use proc_macro2::Span;
-
 extern crate syn;
 use syn::DeriveInput;
 
 #[macro_use]
 extern crate quote;
-
-extern crate heapsize;
-use heapsize::HeapSize;
 
 #[proc_macro_derive(HeapSize)]
 pub fn derive_heap_size(input: TokenStream) -> TokenStream {
@@ -19,11 +13,15 @@ pub fn derive_heap_size(input: TokenStream) -> TokenStream {
     let name = input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-    let expanded = quote_spanned! {
-        Span::def_site(),
-        impl #impl_generics HeapSize for #name #ty_generics #where_clause {
-            fn heap_size_of_children(&self) -> usize {
-                0
+    let expanded = quote! {
+        mod scope {
+            extern crate heapsize;
+            use self::heapsize::HeapSize;
+
+            impl #impl_generics HeapSize for #name #ty_generics #where_clause {
+                fn heap_size_of_children(&self) -> usize {
+                    0
+                }
             }
         }
     };
