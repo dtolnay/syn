ba9f8f65aa45a0dba3afa5bb7f33213a9344af95
David Tolnay <dtolnay@gmail.com>
2018-03-17 13:01:09 -0700

    Ignore redundant_field_names lint to support old compilers

--- src/lib.rs
+++ src/lib.rs
@@ -259,7 +259,7 @@
 #![doc(html_root_url = "https://docs.rs/syn/0.12.14")]
 #![cfg_attr(feature = "cargo-clippy",
             allow(const_static_lifetime, doc_markdown, large_enum_variant, match_bool,
-                  redundant_closure, needless_pass_by_value))]
+                  redundant_closure, needless_pass_by_value, redundant_field_names))]
 
 extern crate proc_macro2;
 extern crate proc_macro;
