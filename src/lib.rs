826fee0d8968cd4d0168cfb7d19e4a1e7d2f5227
David Tolnay <dtolnay@gmail.com>
2018-03-28 13:59:36 +0200

    Release 0.12.15

--- src/lib.rs
+++ src/lib.rs
@@ -256,7 +256,7 @@
 //!   types.
 
 // Syn types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/syn/0.12.14")]
+#![doc(html_root_url = "https://docs.rs/syn/0.12.15")]
 #![cfg_attr(feature = "cargo-clippy",
             allow(const_static_lifetime, doc_markdown, large_enum_variant, match_bool,
                   redundant_closure, needless_pass_by_value, redundant_field_names))]
