1df4ef073bbbcf45ebe511736f8cbc1129455e6d
David Tolnay <dtolnay@gmail.com>
2018-05-12 14:12:52 -0700

    Release 0.13.10

--- src/lib.rs
+++ src/lib.rs
@@ -258,7 +258,7 @@
 //!   dynamic library libproc_macro from rustc toolchain.
 
 // Syn types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/syn/0.13.9")]
+#![doc(html_root_url = "https://docs.rs/syn/0.13.10")]
 #![cfg_attr(
     feature = "cargo-clippy",
     allow(
