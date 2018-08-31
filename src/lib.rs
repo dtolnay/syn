7152d2748f1b5fc016b89e723a2a4c6c36f8d560
David Tolnay <dtolnay@gmail.com>
2018-08-30 17:43:40 -0700

    Remove verbatim helpers

--- src/lib.rs
+++ src/lib.rs
@@ -458,9 +458,6 @@ pub mod parse_quote;
 #[cfg(all(feature = "parsing", feature = "printing"))]
 pub mod spanned;
 
-#[cfg(all(feature = "parsing", feature = "full"))]
-mod verbatim;
-
 mod gen {
     /// Syntax tree traversal to walk a shared borrow of a syntax tree.
     ///
