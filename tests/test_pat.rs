845c8663486d0cf4ad0007b001dabf6cfa9376d9
David Tolnay <dtolnay@gmail.com>
2018-10-20 08:12:25 -0400

    Format with rustfmt 0.99.5-nightly

--- tests/test_pat.rs
+++ tests/test_pat.rs
@@ -12,16 +12,16 @@ extern crate syn;
 
 #[test]
 fn test_pat_ident() {
-  match syn::parse2(quote!(self)).unwrap() {
-    syn::Pat::Ident(_) => (),
-    value => panic!("expected PatIdent, got {:?}", value),
-  }
+    match syn::parse2(quote!(self)).unwrap() {
+        syn::Pat::Ident(_) => (),
+        value => panic!("expected PatIdent, got {:?}", value),
+    }
 }
 
 #[test]
 fn test_pat_path() {
-  match syn::parse2(quote!(self::CONST)).unwrap() {
-    syn::Pat::Path(_) => (),
-    value => panic!("expected PatPath, got {:?}", value),
-  }
+    match syn::parse2(quote!(self::CONST)).unwrap() {
+        syn::Pat::Path(_) => (),
+        value => panic!("expected PatPath, got {:?}", value),
+    }
 }
