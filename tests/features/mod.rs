831ad549f9d0b5137bde66163c67352991c929f5
David Tolnay <dtolnay@gmail.com>
2018-11-06 20:29:20 -0800

    Hide the feature check parse error from rustfmt

--- tests/features/mod.rs
+++ tests/features/mod.rs
@@ -1,3 +1,9 @@
+macro_rules! hide_from_rustfmt {
+    ($($tt:tt)*) => {
+        $($tt)*
+    };
+}
+
 #[cfg(not(all(
     feature = "derive",
     feature = "full",
@@ -10,4 +16,6 @@
     feature = "extra-traits",
     feature = "proc-macro",
 )))]
-mod error;
+hide_from_rustfmt! {
+    mod error;
+}
