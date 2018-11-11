8865861e5f91ee98a09aa6874926b8f60141ee0c
David Tolnay <dtolnay@gmail.com>
2018-11-11 09:52:11 -0800

    Emulate ThreadId on old compilers

--- src/thread.rs
+++ src/thread.rs
@@ -1,60 +1,36 @@
 use std::fmt::{self, Debug};
 
-#[cfg(syn_can_use_thread_id)]
-mod imp {
-    use std::thread::{self, ThreadId};
+use self::thread_id::ThreadId;
 
-    /// ThreadBound is a Sync-maker and Send-maker that allows accessing a value
-    /// of type T only from the original thread on which the ThreadBound was
-    /// constructed.
-    pub struct ThreadBound<T> {
-        value: T,
-        thread_id: ThreadId,
-    }
-
-    unsafe impl<T> Sync for ThreadBound<T> {}
-
-    // Send bound requires Copy, as otherwise Drop could run in the wrong place.
-    unsafe impl<T: Copy> Send for ThreadBound<T> {}
-
-    impl<T> ThreadBound<T> {
-        pub fn new(value: T) -> Self {
-            ThreadBound {
-                value: value,
-                thread_id: thread::current().id(),
-            }
-        }
-
-        pub fn get(&self) -> Option<&T> {
-            if thread::current().id() == self.thread_id {
-                Some(&self.value)
-            } else {
-                None
-            }
-        }
-    }
+/// ThreadBound is a Sync-maker and Send-maker that allows accessing a value
+/// of type T only from the original thread on which the ThreadBound was
+/// constructed.
+pub struct ThreadBound<T> {
+    value: T,
+    thread_id: ThreadId,
 }
 
-#[cfg(not(syn_can_use_thread_id))]
-mod imp {
-    pub struct ThreadBound<T> {
-        value: T,
+unsafe impl<T> Sync for ThreadBound<T> {}
+
+// Send bound requires Copy, as otherwise Drop could run in the wrong place.
+unsafe impl<T: Copy> Send for ThreadBound<T> {}
+
+impl<T> ThreadBound<T> {
+    pub fn new(value: T) -> Self {
+        ThreadBound {
+            value: value,
+            thread_id: thread_id::current(),
+        }
     }
 
-    impl<T> ThreadBound<T> {
-        pub fn new(value: T) -> Self {
-            ThreadBound {
-                value: value,
-            }
-        }
-
-        pub fn get(&self) -> Option<&T> {
+    pub fn get(&self) -> Option<&T> {
+        if thread_id::current() == self.thread_id {
             Some(&self.value)
+        } else {
+            None
         }
     }
 }
-
-pub use self::imp::ThreadBound;
 
 impl<T: Debug> Debug for ThreadBound<T> {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -62,5 +38,34 @@ impl<T: Debug> Debug for ThreadBound<T> {
             Some(value) => Debug::fmt(value, formatter),
             None => formatter.write_str("unknown"),
         }
+    }
+}
+
+#[cfg(syn_can_use_thread_id)]
+mod thread_id {
+    use std::thread;
+
+    pub use std::thread::ThreadId;
+
+    pub fn current() -> ThreadId {
+        thread::current().id()
+    }
+}
+
+#[cfg(not(syn_can_use_thread_id))]
+mod thread_id {
+    use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+
+    thread_local! {
+        static THREAD_ID: usize = {
+            static NEXT_THREAD_ID: AtomicUsize = ATOMIC_USIZE_INIT;
+            NEXT_THREAD_ID.fetch_add(1, Ordering::Relaxed)
+        };
+    }
+
+    pub type ThreadId = usize;
+
+    pub fn current() -> ThreadId {
+        THREAD_ID.with(|id| *id)
     }
 }
