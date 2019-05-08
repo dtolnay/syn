35c8653139d260c84fb77c767636b9ff7a5ff3a0
David Tolnay <dtolnay@gmail.com>
2019-05-07 21:54:43 -0700

    Avoid importing parse2 in tests since we don't in docs

--- tests/test_expr.rs
+++ tests/test_expr.rs
@@ -4,13 +4,13 @@ extern crate syn;
 use std::str::FromStr;
 
 use proc_macro2::TokenStream;
-use syn::{Expr, parse2};
+use syn::{Expr, ExprRange};
 
 #[test]
 fn test_expr_parse() {
     let code = "..100u32";
     let tt = TokenStream::from_str(code).unwrap();
-    let ast1: Expr = parse2(tt.clone()).unwrap();
-    let ast2: syn::ExprRange = parse2(tt).unwrap();
+    let ast1: Expr = syn::parse2(tt.clone()).unwrap();
+    let ast2: ExprRange = syn::parse2(tt).unwrap();
     assert_eq!(ast1, Expr::Range(ast2));
 }
