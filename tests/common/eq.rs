1150d037aa56a896b2dd54f7f6b1450aaf2cda21
David Tolnay <dtolnay@gmail.com>
2019-05-25 00:05:38 -0700

    Update test suite to nightly-2019-05-25

--- tests/common/eq.rs
+++ tests/common/eq.rs
@@ -360,13 +360,13 @@ spanless_eq_enum!(UseTreeKind; Simple(0 1 2) Nested(0) Glob);
 spanless_eq_enum!(VariantData; Struct(0 1) Tuple(0 1) Unit(0));
 spanless_eq_enum!(VisibilityKind; Public Crate(0) Restricted(path id) Inherited);
 spanless_eq_enum!(WherePredicate; BoundPredicate(0) RegionPredicate(0) EqPredicate(0));
-spanless_eq_enum!(ExprKind; Box(0) ObsoleteInPlace(0 1) Array(0) Call(0 1)
-    MethodCall(0 1) Tup(0) Binary(0 1 2) Unary(0 1) Lit(0) Cast(0 1) Type(0 1)
-    If(0 1 2) IfLet(0 1 2 3) While(0 1 2) WhileLet(0 1 2 3) ForLoop(0 1 2 3)
-    Loop(0 1) Match(0 1) Closure(0 1 2 3 4 5) Block(0 1) Async(0 1 2) Await(0 1)
-    TryBlock(0) Assign(0 1) AssignOp(0 1 2) Field(0 1) Index(0 1) Range(0 1 2)
-    Path(0 1) AddrOf(0 1) Break(0 1) Continue(0) Ret(0) InlineAsm(0) Mac(0)
-    Struct(0 1 2) Repeat(0 1) Paren(0) Try(0) Yield(0) Err);
+spanless_eq_enum!(ExprKind; Box(0) Array(0) Call(0 1) MethodCall(0 1) Tup(0)
+    Binary(0 1 2) Unary(0 1) Lit(0) Cast(0 1) Type(0 1) If(0 1 2) IfLet(0 1 2 3)
+    While(0 1 2) WhileLet(0 1 2 3) ForLoop(0 1 2 3) Loop(0 1) Match(0 1)
+    Closure(0 1 2 3 4 5) Block(0 1) Async(0 1 2) Await(0 1) TryBlock(0)
+    Assign(0 1) AssignOp(0 1 2) Field(0 1) Index(0 1) Range(0 1 2) Path(0 1)
+    AddrOf(0 1) Break(0 1) Continue(0) Ret(0) InlineAsm(0) Mac(0) Struct(0 1 2)
+    Repeat(0 1) Paren(0) Try(0) Yield(0) Err);
 spanless_eq_enum!(ItemKind; ExternCrate(0) Use(0) Static(0 1 2) Const(0 1)
     Fn(0 1 2 3) Mod(0) ForeignMod(0) GlobalAsm(0) Ty(0 1) Existential(0 1)
     Enum(0 1) Struct(0 1) Union(0 1) Trait(0 1 2 3 4) TraitAlias(0 1)
