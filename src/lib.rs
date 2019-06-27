49a674c76e19c41a26210b47f72ddad1e425ff34
David Tolnay <dtolnay@gmail.com>
2019-06-27 00:01:20 -0700

    Release 0.15.39

--- src/lib.rs
+++ src/lib.rs
@@ -235,7 +235,7 @@
 //!   dynamic library libproc_macro from rustc toolchain.
 
 // Syn types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/syn/0.15.38")]
+#![doc(html_root_url = "https://docs.rs/syn/0.15.39")]
 #![allow(unknown_lints, bare_trait_objects, ellipsis_inclusive_range_patterns)]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
