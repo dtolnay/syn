3b6361be884ef76370728444d59c5c240f48cc80
David Tolnay <dtolnay@gmail.com>
2019-07-29 08:32:03 -0700

    Implement bigint AddAssign

--- src/bigint.rs
+++ src/bigint.rs
@@ -15,12 +15,28 @@ impl BigInt {
     pub fn to_string(&self) -> String {
         unimplemented!()
     }
+
+    fn reserve_two_digits(&mut self) {
+        let len = self.digits.len();
+        let desired = len
+            + !self.digits.ends_with(&[0, 0]) as usize
+            + !self.digits.ends_with(&[0]) as usize;
+        self.digits.resize(desired, 0);
+    }
 }
 
 impl AddAssign<u8> for BigInt {
-    fn add_assign(&mut self, increment: u8) {
-        let _ = increment;
-        unimplemented!()
+    // Assumes increment <16.
+    fn add_assign(&mut self, mut increment: u8) {
+        self.reserve_two_digits();
+
+        let mut i = 0;
+        while increment > 0 {
+            let sum = self.digits[i] + increment;
+            self.digits[i] = sum % 10;
+            increment = sum / 10;
+            i += 1;
+        }
     }
 }
 
