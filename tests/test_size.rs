e38c0fd061169a2fd0aae0f56c01918fa89afc66
David Tolnay <dtolnay@gmail.com>
2019-07-28 17:42:38 -0700

    Track syntax tree node size in bytes

---
+++ tests/test_size.rs
@@ -0,0 +1,26 @@
+#![cfg(target_pointer_width = "64")]
+
+mod features;
+
+use std::mem;
+use syn::*;
+
+#[test]
+fn test_expr_size() {
+    assert_eq!(mem::size_of::<Expr>(), 176);
+}
+
+#[test]
+fn test_item_size() {
+    assert_eq!(mem::size_of::<Item>(), 280);
+}
+
+#[test]
+fn test_type_size() {
+    assert_eq!(mem::size_of::<Type>(), 200);
+}
+
+#[test]
+fn test_pat_size() {
+    assert_eq!(mem::size_of::<Pat>(), 248);
+}
