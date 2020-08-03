1e31fdffdca2a1389236ee4434b6580b8190ac38
David Tolnay <dtolnay@gmail.com>
2020-08-02 22:37:25 -0700

    Respect RUST_MIN_STACK initializing Rayon threadpool

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -16,8 +16,12 @@ pub fn abort_after() -> usize {
 
 /// Configure Rayon threadpool.
 pub fn rayon_init() {
+    let stack_size = match env::var("RUST_MIN_STACK") {
+        Ok(s) => s.parse().expect("failed to parse RUST_MIN_STACK"),
+        Err(_) => 20 * 1024 * 1024,
+    };
     ThreadPoolBuilder::new()
-        .stack_size(20 * 1024 * 1024)
+        .stack_size(stack_size)
         .build_global()
         .unwrap();
 }
