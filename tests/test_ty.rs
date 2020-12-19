5822aeabaad20a71262bef3405cd945398d44dcc
David Tolnay <dtolnay@gmail.com>
2020-12-18 18:35:02 -0800

    Add test of path type wrapped in None-delimited group

--- tests/test_ty.rs
+++ tests/test_ty.rs
@@ -51,3 +51,53 @@ fn test_macro_variable_type() {
     }
     "###);
 }
+
+#[test]
+fn test_group_angle_brackets() {
+    // mimics the token stream corresponding to `Option<$ty>`
+    let tokens = TokenStream::from_iter(vec![
+        TokenTree::Ident(Ident::new("Option", Span::call_site())),
+        TokenTree::Punct(Punct::new('<', Spacing::Alone)),
+        TokenTree::Group(Group::new(Delimiter::None, quote! { Vec<u8> })),
+        TokenTree::Punct(Punct::new('>', Spacing::Alone)),
+    ]);
+
+    snapshot!(tokens as Type, @r###"
+    Type::Path {
+        path: Path {
+            segments: [
+                PathSegment {
+                    ident: "Option",
+                    arguments: PathArguments::AngleBracketed {
+                        args: [
+                            Type(Type::Path {
+                                path: Path {
+                                    segments: [
+                                        PathSegment {
+                                            ident: "Vec",
+                                            arguments: PathArguments::AngleBracketed {
+                                                args: [
+                                                    Type(Type::Path {
+                                                        path: Path {
+                                                            segments: [
+                                                                PathSegment {
+                                                                    ident: "u8",
+                                                                    arguments: None,
+                                                                },
+                                                            ],
+                                                        },
+                                                    }),
+                                                ],
+                                            },
+                                        },
+                                    ],
+                                },
+                            }),
+                        ],
+                    },
+                },
+            ],
+        },
+    }
+    "###);
+}
