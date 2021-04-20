8572f0669b6876e7cd827f764b35ce743c4532cb
Gabriel Facco de Arruda <arrudagates@gmail.com>
2021-04-20 15:44:10 -0300

    Fixed a tiny typo
    line 43: infinte -> infinite

--- src/lit.rs
+++ src/lit.rs
@@ -40,7 +40,7 @@ ast_enum_of_structs! {
 
         /// A floating point literal: `1f64` or `1.0e10f64`.
         ///
-        /// Must be finite. May not be infinte or NaN.
+        /// Must be finite. May not be infinite or NaN.
         Float(LitFloat),
 
         /// A boolean literal: `true` or `false`.
