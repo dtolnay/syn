9e206dad2346e2f4542abb2200c397ceefddfc06
David Tolnay <dtolnay@gmail.com>
2021-07-21 15:00:51 -0700

    Release 1.0.74

--- src/lib.rs
+++ src/lib.rs
@@ -250,7 +250,7 @@
 //!   dynamic library libproc_macro from rustc toolchain.
 
 // Syn types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/syn/1.0.73")]
+#![doc(html_root_url = "https://docs.rs/syn/1.0.74")]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 #![allow(non_camel_case_types)]
 // Ignored clippy lints.
