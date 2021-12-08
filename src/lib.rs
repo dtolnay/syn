7bf8df69b30a5f360417212fc48f14f527a67786
David Tolnay <dtolnay@gmail.com>
2021-12-07 18:53:29 -0800

    Ignore cast_lossless Clippy pedantic lint
        error: casting `bool` to `usize` is more cleanly stated with `usize::from(_)`
          --> src/bigint.rs:34:19
           |
        34 |             len + !self.digits.ends_with(&[0, 0]) as usize + !self.digits.ends_with(&[0]) as usize;
           |                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try: `usize::from(!self.digits.ends_with(&[0, 0]))`
           |
           = note: `-D clippy::cast-lossless` implied by `-D clippy::pedantic`
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#cast_lossless
    
        error: casting `bool` to `usize` is more cleanly stated with `usize::from(_)`
          --> src/bigint.rs:34:62
           |
        34 |             len + !self.digits.ends_with(&[0, 0]) as usize + !self.digits.ends_with(&[0]) as usize;
           |                                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try: `usize::from(!self.digits.ends_with(&[0]))`
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#cast_lossless
    
        error: casting `bool` to `usize` is more cleanly stated with `usize::from(_)`
            --> src/lit.rs:1458:21
             |
        1458 |         let start = (*bytes.get(0)? == b'-') as usize;
             |                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try: `usize::from(*bytes.get(0)? == b'-')`
             |
             = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#cast_lossless

--- src/lib.rs
+++ src/lib.rs
@@ -255,6 +255,7 @@
 #![allow(non_camel_case_types)]
 // Ignored clippy lints.
 #![allow(
+    clippy::cast_lossless,
     clippy::collapsible_match, // https://github.com/rust-lang/rust-clippy/issues/7575
     clippy::doc_markdown,
     clippy::eval_order_dependence,
