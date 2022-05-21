0910d57f5a64c9c79a1359069c11396ba58af38e
David Tolnay <dtolnay@gmail.com>
2022-05-20 21:57:02 -0700

    Update test suite to nightly-2022-05-21

--- tests/common/eq.rs
+++ tests/common/eq.rs
@@ -303,7 +303,7 @@ spanless_eq_struct!(AttrAnnotatedTokenStream; 0);
 spanless_eq_struct!(AttrItem; path args tokens);
 spanless_eq_struct!(Attribute; kind id style span);
 spanless_eq_struct!(AttributesData; attrs tokens);
-spanless_eq_struct!(BareFnTy; unsafety ext generic_params decl);
+spanless_eq_struct!(BareFnTy; unsafety ext generic_params decl decl_span);
 spanless_eq_struct!(Block; stmts id rules span tokens could_be_bare_literal);
 spanless_eq_struct!(Crate; attrs items spans id is_placeholder);
 spanless_eq_struct!(EnumDef; variants);
