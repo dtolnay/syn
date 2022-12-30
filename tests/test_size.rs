cbf321fc30e6cb443a0b730d61d1d961bc5d5da2
David Tolnay <dtolnay@gmail.com>
2022-12-30 12:51:35 -0800

    Disable test_size tests on old nightlies with different size

--- tests/test_size.rs
+++ tests/test_size.rs
@@ -3,26 +3,31 @@
 use std::mem;
 use syn::{Expr, Item, Lit, Pat, Type};
 
+#[rustversion::attr(before(2022-11-24), ignore)]
 #[test]
 fn test_expr_size() {
     assert_eq!(mem::size_of::<Expr>(), 272);
 }
 
+#[rustversion::attr(before(2022-09-09), ignore)]
 #[test]
 fn test_item_size() {
     assert_eq!(mem::size_of::<Item>(), 320);
 }
 
+#[rustversion::attr(before(2022-11-24), ignore)]
 #[test]
 fn test_type_size() {
     assert_eq!(mem::size_of::<Type>(), 288);
 }
 
+#[rustversion::attr(before(2021-10-11), ignore)]
 #[test]
 fn test_pat_size() {
     assert_eq!(mem::size_of::<Pat>(), 144);
 }
 
+#[rustversion::attr(before(2022-09-09), ignore)]
 #[test]
 fn test_lit_size() {
     assert_eq!(mem::size_of::<Lit>(), 32);
