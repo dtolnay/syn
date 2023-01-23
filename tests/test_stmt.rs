9a14d7e0546232aa73f2220da634f446eac5fa7c
David Tolnay <dtolnay@gmail.com>
2023-01-23 13:05:12 -0800

    Add test of parsing macros in stmt position

--- tests/test_stmt.rs
+++ tests/test_stmt.rs
@@ -155,3 +155,93 @@ fn test_let_else() {
     })
     "###);
 }
+
+#[test]
+fn test_macros() {
+    let tokens = quote! {
+        fn main() {
+            macro_rules! mac {}
+            thread_local! { static FOO }
+            println!("");
+            vec![]
+        }
+    };
+
+    snapshot!(tokens as Stmt, @r###"
+    Item(Item::Fn {
+        vis: Inherited,
+        sig: Signature {
+            ident: "main",
+            generics: Generics,
+            output: Default,
+        },
+        block: Block {
+            stmts: [
+                Item(Item::Macro {
+                    ident: Some("mac"),
+                    mac: Macro {
+                        path: Path {
+                            segments: [
+                                PathSegment {
+                                    ident: "macro_rules",
+                                    arguments: None,
+                                },
+                            ],
+                        },
+                        delimiter: Brace,
+                        tokens: TokenStream(``),
+                    },
+                }),
+                Item(Item::Macro {
+                    mac: Macro {
+                        path: Path {
+                            segments: [
+                                PathSegment {
+                                    ident: "thread_local",
+                                    arguments: None,
+                                },
+                            ],
+                        },
+                        delimiter: Brace,
+                        tokens: TokenStream(`static FOO`),
+                    },
+                }),
+                Expr(
+                    Expr::Macro {
+                        mac: Macro {
+                            path: Path {
+                                segments: [
+                                    PathSegment {
+                                        ident: "println",
+                                        arguments: None,
+                                    },
+                                ],
+                            },
+                            delimiter: Paren,
+                            tokens: TokenStream(`""`),
+                        },
+                    },
+                    Some,
+                ),
+                Expr(
+                    Expr::Macro {
+                        mac: Macro {
+                            path: Path {
+                                segments: [
+                                    PathSegment {
+                                        ident: "vec",
+                                        arguments: None,
+                                    },
+                                ],
+                            },
+                            delimiter: Bracket,
+                            tokens: TokenStream(``),
+                        },
+                    },
+                    None,
+                ),
+            ],
+        },
+    })
+    "###);
+}
