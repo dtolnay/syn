bbf57611ebfe952edf8304b6d314fa381ee66eaa
David Tolnay <dtolnay@gmail.com>
2023-02-06 21:52:07 -0800

    Convert tests of receiver() to snapshot tests

--- tests/test_receiver.rs
+++ tests/test_receiver.rs
@@ -1,16 +1,31 @@
 #![allow(clippy::uninlined_format_args)]
 
-use syn::{parse_quote, FnArg, Receiver, TraitItemFn};
+#[macro_use]
+mod macros;
+
+use syn::{parse_quote, TraitItemFn};
 
 #[test]
 fn test_by_value() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn by_value(self: Self);
     };
-    match sig.receiver() {
-        Some(FnArg::Typed(_)) => (),
-        value => panic!("expected FnArg::Typed, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Typed(PatType {
+        pat: Pat::Ident {
+            ident: "self",
+        },
+        ty: Type::Path {
+            path: Path {
+                segments: [
+                    PathSegment {
+                        ident: "Self",
+                    },
+                ],
+            },
+        },
+    })
+    "###);
 }
 
 #[test]
@@ -18,10 +33,23 @@ fn test_by_mut_value() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn by_mut(mut self: Self);
     };
-    match sig.receiver() {
-        Some(FnArg::Typed(_)) => (),
-        value => panic!("expected FnArg::Typed, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Typed(PatType {
+        pat: Pat::Ident {
+            mutability: Some,
+            ident: "self",
+        },
+        ty: Type::Path {
+            path: Path {
+                segments: [
+                    PathSegment {
+                        ident: "Self",
+                    },
+                ],
+            },
+        },
+    })
+    "###);
 }
 
 #[test]
@@ -29,10 +57,24 @@ fn test_by_ref() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn by_ref(self: &Self);
     };
-    match sig.receiver() {
-        Some(FnArg::Typed(_)) => (),
-        value => panic!("expected FnArg::Typed, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Typed(PatType {
+        pat: Pat::Ident {
+            ident: "self",
+        },
+        ty: Type::Reference {
+            elem: Type::Path {
+                path: Path {
+                    segments: [
+                        PathSegment {
+                            ident: "Self",
+                        },
+                    ],
+                },
+            },
+        },
+    })
+    "###);
 }
 
 #[test]
@@ -40,10 +82,35 @@ fn test_by_box() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn by_box(self: Box<Self>);
     };
-    match sig.receiver() {
-        Some(FnArg::Typed(_)) => (),
-        value => panic!("expected FnArg::Typed, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Typed(PatType {
+        pat: Pat::Ident {
+            ident: "self",
+        },
+        ty: Type::Path {
+            path: Path {
+                segments: [
+                    PathSegment {
+                        ident: "Box",
+                        arguments: PathArguments::AngleBracketed {
+                            args: [
+                                GenericArgument::Type(Type::Path {
+                                    path: Path {
+                                        segments: [
+                                            PathSegment {
+                                                ident: "Self",
+                                            },
+                                        ],
+                                    },
+                                }),
+                            ],
+                        },
+                    },
+                ],
+            },
+        },
+    })
+    "###);
 }
 
 #[test]
@@ -51,10 +118,35 @@ fn test_by_pin() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn by_pin(self: Pin<Self>);
     };
-    match sig.receiver() {
-        Some(FnArg::Typed(_)) => (),
-        value => panic!("expected FnArg::Typed, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Typed(PatType {
+        pat: Pat::Ident {
+            ident: "self",
+        },
+        ty: Type::Path {
+            path: Path {
+                segments: [
+                    PathSegment {
+                        ident: "Pin",
+                        arguments: PathArguments::AngleBracketed {
+                            args: [
+                                GenericArgument::Type(Type::Path {
+                                    path: Path {
+                                        segments: [
+                                            PathSegment {
+                                                ident: "Self",
+                                            },
+                                        ],
+                                    },
+                                }),
+                            ],
+                        },
+                    },
+                ],
+            },
+        },
+    })
+    "###);
 }
 
 #[test]
@@ -62,10 +154,35 @@ fn test_explicit_type() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn explicit_type(self: Pin<MyType>);
     };
-    match sig.receiver() {
-        Some(FnArg::Typed(_)) => (),
-        value => panic!("expected FnArg::Typed, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Typed(PatType {
+        pat: Pat::Ident {
+            ident: "self",
+        },
+        ty: Type::Path {
+            path: Path {
+                segments: [
+                    PathSegment {
+                        ident: "Pin",
+                        arguments: PathArguments::AngleBracketed {
+                            args: [
+                                GenericArgument::Type(Type::Path {
+                                    path: Path {
+                                        segments: [
+                                            PathSegment {
+                                                ident: "MyType",
+                                            },
+                                        ],
+                                    },
+                                }),
+                            ],
+                        },
+                    },
+                ],
+            },
+        },
+    })
+    "###);
 }
 
 #[test]
@@ -73,14 +190,7 @@ fn test_value_shorthand() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn value_shorthand(self);
     };
-    match sig.receiver() {
-        Some(FnArg::Receiver(Receiver {
-            reference: None,
-            mutability: None,
-            ..
-        })) => (),
-        value => panic!("expected FnArg::Receiver without ref/mut, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @"FnArg::Receiver(Receiver)");
 }
 
 #[test]
@@ -88,14 +198,11 @@ fn test_mut_value_shorthand() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn mut_value_shorthand(mut self);
     };
-    match sig.receiver() {
-        Some(FnArg::Receiver(Receiver {
-            reference: None,
-            mutability: Some(_),
-            ..
-        })) => (),
-        value => panic!("expected FnArg::Receiver with mut, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Receiver(Receiver {
+        mutability: Some,
+    })
+    "###);
 }
 
 #[test]
@@ -103,14 +210,11 @@ fn test_ref_shorthand() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn ref_shorthand(&self);
     };
-    match sig.receiver() {
-        Some(FnArg::Receiver(Receiver {
-            reference: Some(_),
-            mutability: None,
-            ..
-        })) => (),
-        value => panic!("expected FnArg::Receiver with ref, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Receiver(Receiver {
+        reference: Some(None),
+    })
+    "###);
 }
 
 #[test]
@@ -118,12 +222,10 @@ fn test_ref_mut_shorthand() {
     let TraitItemFn { sig, .. } = parse_quote! {
         fn ref_mut_shorthand(&mut self);
     };
-    match sig.receiver() {
-        Some(FnArg::Receiver(Receiver {
-            reference: Some(_),
-            mutability: Some(_),
-            ..
-        })) => (),
-        value => panic!("expected FnArg::Receiver with ref+mut, got {:?}", value),
-    }
+    snapshot!(&sig.inputs[0], @r###"
+    FnArg::Receiver(Receiver {
+        reference: Some(None),
+        mutability: Some,
+    })
+    "###);
 }
