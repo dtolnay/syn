dffe82b26ee2c456e47275fccb6112b9d160e717
David Tolnay <dtolnay@gmail.com>
2023-03-26 13:51:02 -0700

    Perform build-time alerts in test suite using build-alert crate

--- tests/features/lib.rs
+++ tests/features/lib.rs
@@ -1,1 +1,16 @@
-syn_test_suite_feature_check::check!();
+#[cfg(debug_assertions)]
+build_alert::yellow! {"
+NOTE:  use --release
+  Syn's test suite has some tests that run on every source file
+  and test case in the rust-lang/rust repo, which can be pretty
+  slow in debug mode. Consider running cargo test with `--release`
+  to speed things up.
+"}
+
+#[cfg(not(feature = "all-features"))]
+build_alert::red! {"
+ERROR:  use --all-features
+  Syn's test suite normally only works with all-features enabled.
+  Run again with `--all-features`, or run with `--features test`
+  to bypass this check.
+"}
