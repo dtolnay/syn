1c1c2f074fec3297e41b4e2ba70316fd2ce28d24
David Tolnay <dtolnay@gmail.com>
2023-04-02 22:38:04 -0700

    Release syn-codegen 0.3.1

--- json/src/lib.rs
+++ json/src/lib.rs
@@ -44,7 +44,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/syn-codegen/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/syn-codegen/0.3.1")]
 
 use indexmap::IndexMap;
 use semver::Version;
