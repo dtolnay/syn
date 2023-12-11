ede96cdb1b219c775eade1c0d3559693811e72df
David Tolnay <dtolnay@gmail.com>
2023-12-10 17:59:49 -0800

    Add test reproducing unary None-delimited group precedence bug

--- tests/test_expr.rs
+++ tests/test_expr.rs
@@ -217,6 +217,30 @@ fn test_macro_variable_struct() {
 }
 
 #[test]
+fn test_macro_variable_unary() {
+    // mimics the token stream corresponding to `$expr.method()` where expr is `&self`
+    let inner = Group::new(Delimiter::None, quote!(&self));
+    let tokens = quote!(#inner.method());
+    // FIXME
+    snapshot!(tokens as Expr, @r###"
+    Expr::Reference {
+        expr: Expr::MethodCall {
+            receiver: Expr::Path {
+                path: Path {
+                    segments: [
+                        PathSegment {
+                            ident: "self",
+                        },
+                    ],
+                },
+            },
+            method: "method",
+        },
+    }
+    "###);
+}
+
+#[test]
 fn test_macro_variable_match_arm() {
     // mimics the token stream corresponding to `match v { _ => $expr }`
     let tokens = TokenStream::from_iter(vec![
