82f1eb74a94648469ecf4c55fd525d146f52a45b
David Tolnay <dtolnay@gmail.com>
2024-01-01 10:31:25 -0800

    Ignore diverging_sub_expression clippy lint
        warning: sub-expression diverges
           --> src/data.rs:263:47
            |
        263 |                     initial = consume![if] || break;
            |                                               ^^^^^
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#diverging_sub_expression
            = note: `-W clippy::diverging-sub-expression` implied by `-W clippy::all`
            = help: to override `-W clippy::all` add `#[allow(clippy::diverging_sub_expression)]`

--- src/lib.rs
+++ src/lib.rs
@@ -261,6 +261,7 @@
     clippy::cast_ptr_alignment,
     clippy::default_trait_access,
     clippy::derivable_impls,
+    clippy::diverging_sub_expression,
     clippy::doc_markdown,
     clippy::expl_impl_clone_on_copy,
     clippy::explicit_auto_deref,
