8abde8407f8e4300f242d760a9d0a80c9d91560b
David Tolnay <dtolnay@gmail.com>
2024-04-14 14:59:31 -0700

    Remove codegen's dependency on serde_derive

--- codegen/src/version.rs
+++ codegen/src/version.rs
@@ -1,7 +1,8 @@
 use crate::workspace_path;
 use anyhow::Result;
 use semver::Version;
-use serde_derive::Deserialize;
+use serde::de::{Deserialize, Deserializer, IgnoredAny, MapAccess, Visitor};
+use std::fmt;
 use std::fs;
 
 pub fn get() -> Result<Version> {
@@ -11,12 +12,88 @@ pub fn get() -> Result<Version> {
     Ok(parsed.package.version)
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct Manifest {
     package: Package,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct Package {
     version: Version,
+}
+
+impl<'de> Deserialize<'de> for Manifest {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct ManifestVisitor;
+
+        impl<'de> Visitor<'de> for ManifestVisitor {
+            type Value = Manifest;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct Manifest")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'de>,
+            {
+                let mut package = None;
+                while let Some(k) = map.next_key::<String>()? {
+                    if k == "package" {
+                        if package.is_some() {
+                            return Err(serde::de::Error::duplicate_field("package"));
+                        }
+                        package = Some(map.next_value()?);
+                    } else {
+                        map.next_value::<IgnoredAny>()?;
+                    }
+                }
+                let package = package.ok_or_else(|| serde::de::Error::missing_field("package"))?;
+                Ok(Manifest { package })
+            }
+        }
+
+        deserializer.deserialize_struct("Manifest", &["package"], ManifestVisitor)
+    }
+}
+
+impl<'de> Deserialize<'de> for Package {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct PackageVisitor;
+
+        impl<'de> Visitor<'de> for PackageVisitor {
+            type Value = Package;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct Package")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'de>,
+            {
+                let mut version = None;
+                while let Some(k) = map.next_key::<String>()? {
+                    if k == "version" {
+                        if version.is_some() {
+                            return Err(serde::de::Error::duplicate_field("version"));
+                        }
+                        version = Some(map.next_value()?);
+                    } else {
+                        map.next_value::<IgnoredAny>()?;
+                    }
+                }
+                let version = version.ok_or_else(|| serde::de::Error::missing_field("version"))?;
+                Ok(Package { version })
+            }
+        }
+
+        deserializer.deserialize_struct("Package", &["version"], PackageVisitor)
+    }
 }
