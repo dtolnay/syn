82ffe86c2b721b9985edb6f368e7366bd202bc5b
David Tolnay <dtolnay@gmail.com>
2024-05-19 12:13:13 -0700

    Move Punctuated fold helper to punctuated module

--- src/punctuated.rs
+++ src/punctuated.rs
@@ -1073,6 +1073,25 @@ impl<T, P> IndexMut<usize> for Punctuated<T, P> {
     }
 }
 
+#[cfg(all(feature = "fold", any(feature = "full", feature = "derive")))]
+pub(crate) fn fold<T, P, V, F>(
+    punctuated: Punctuated<T, P>,
+    fold: &mut V,
+    mut f: F,
+) -> Punctuated<T, P>
+where
+    V: ?Sized,
+    F: FnMut(&mut V, T) -> T,
+{
+    punctuated
+        .into_pairs()
+        .map(|pair| match pair {
+            Pair::Punctuated(t, p) => Pair::Punctuated(f(fold, t), p),
+            Pair::End(t) => Pair::End(f(fold, t)),
+        })
+        .collect()
+}
+
 #[cfg(feature = "printing")]
 mod printing {
     use crate::punctuated::{Pair, Punctuated};
