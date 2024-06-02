a0967ad273e98909c2585c8b1e629ed08f7ba26c
David Tolnay <dtolnay@gmail.com>
2024-06-01 20:47:02 -0700

    Make size tests ignored on 32-bit, not compiled out

--- tests/test_size.rs
+++ tests/test_size.rs
@@ -1,35 +1,38 @@
 // Assumes proc-macro2's "span-locations" feature is off.
 
-#![cfg(target_pointer_width = "64")]
-
 use std::mem;
 use syn::{Expr, Item, Lit, Pat, Type};
 
 #[rustversion::attr(before(2022-11-24), ignore)]
+#[rustversion::attr(since(2022-11-24), cfg_attr(not(target_pointer_width = "64"), ignore))]
 #[test]
 fn test_expr_size() {
     assert_eq!(mem::size_of::<Expr>(), 176);
 }
 
 #[rustversion::attr(before(2022-09-09), ignore)]
+#[rustversion::attr(since(2022-09-09), cfg_attr(not(target_pointer_width = "64"), ignore))]
 #[test]
 fn test_item_size() {
     assert_eq!(mem::size_of::<Item>(), 352);
 }
 
 #[rustversion::attr(before(2023-04-29), ignore)]
+#[rustversion::attr(since(2023-04-29), cfg_attr(not(target_pointer_width = "64"), ignore))]
 #[test]
 fn test_type_size() {
     assert_eq!(mem::size_of::<Type>(), 224);
 }
 
 #[rustversion::attr(before(2023-04-29), ignore)]
+#[rustversion::attr(since(2023-04-29), cfg_attr(not(target_pointer_width = "64"), ignore))]
 #[test]
 fn test_pat_size() {
     assert_eq!(mem::size_of::<Pat>(), 184);
 }
 
 #[rustversion::attr(before(2023-12-20), ignore)]
+#[rustversion::attr(since(2023-12-20), cfg_attr(not(target_pointer_width = "64"), ignore))]
 #[test]
 fn test_lit_size() {
     assert_eq!(mem::size_of::<Lit>(), 24);
