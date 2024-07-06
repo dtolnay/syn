a89a1b69efc003fd6b3b309a5317160dd0e90376
David Tolnay <dtolnay@gmail.com>
2024-07-06 15:29:13 -0700

    Add new fixup tests for continuation of a jump expr

--- tests/test_expr.rs
+++ tests/test_expr.rs
@@ -674,6 +674,8 @@ fn test_fixup() {
         quote! { if let _ = (S {}) {} },
         quote! { break ('a: loop { break 'a 1 } + 1) },
         quote! { a + (|| b) + c },
+        quote! { if let _ = ((break) - 1 || true) {} },
+        quote! { if let _ = ((break) + 1 || true) {} },
     ] {
         let original: Expr = syn::parse2(tokens).unwrap();
 
