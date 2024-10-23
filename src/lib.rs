dfc937235d85fa73d8348da3c8f0be9c64ad4b8a
David Tolnay <dtolnay@gmail.com>
2024-10-23 10:52:56 -0700

    Release 2.0.83

--- src/lib.rs
+++ src/lib.rs
@@ -249,7 +249,7 @@
 //!   dynamic library libproc_macro from rustc toolchain.
 
 // Syn types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/syn/2.0.82")]
+#![doc(html_root_url = "https://docs.rs/syn/2.0.83")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(non_camel_case_types)]
