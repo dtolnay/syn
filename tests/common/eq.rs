bbe73233a18251da08f148050e42b804a49c0373
David Tolnay <dtolnay@gmail.com>
2025-07-02 21:38:51 -0700

    Update test suite to nightly-2025-07-03

--- tests/common/eq.rs
+++ tests/common/eq.rs
@@ -616,7 +616,7 @@ spanless_eq_enum!(StrStyle; Cooked Raw(0));
 spanless_eq_enum!(StructRest; Base(0) Rest(0) None);
 spanless_eq_enum!(Term; Ty(0) Const(0));
 spanless_eq_enum!(TokenTree; Token(0 1) Delimited(0 1 2 3));
-spanless_eq_enum!(TraitObjectSyntax; Dyn DynStar None);
+spanless_eq_enum!(TraitObjectSyntax; Dyn None);
 spanless_eq_enum!(TyPatKind; Range(0 1 2) Or(0) Err(0));
 spanless_eq_enum!(UintTy; Usize U8 U16 U32 U64 U128);
 spanless_eq_enum!(UnOp; Deref Not Neg);
