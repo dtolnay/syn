888a207102d6fdc74aa1ddbc64f215a02e297a2b
David Tolnay <dtolnay@gmail.com>
2026-01-05 14:24:45 -0800

    Delete html_root_url comment

--- src/lib.rs
+++ src/lib.rs
@@ -248,7 +248,6 @@
 //! - **`proc-macro`** *(enabled by default)* — Runtime dependency on the
 //!   dynamic library libproc_macro from rustc toolchain.
 
-// Syn types in rustdoc of other crates get linked to here.
 #![doc(html_root_url = "https://docs.rs/syn/2.0.113")]
 #![cfg_attr(docsrs, feature(doc_cfg), doc(auto_cfg = false))]
 #![deny(unsafe_op_in_unsafe_fn)]
